6520d0ddba251645c2ae439e172b9419c017ca7d
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2021-10-26 22:20:08 +0100

    make the cells being a set instead of a list

--- FunctionalProgramming/Recursion/crosswords-101.hs
+++ FunctionalProgramming/Recursion/crosswords-101.hs
@@ -1,14 +1,19 @@
-import Data.Char (isAlpha)
+import Data.Char  (isAlpha)
+import Data.List  (nub)
+import Data.Maybe (mapMaybe)
+import Data.Set   (Set)
+
+import qualified Data.Set as Set
 
 newtype Coordinate = Coordinate
-  { coordinateValue :: (Int, Int) } deriving (Eq)
+  { coordinateValue :: (Int, Int) } deriving (Eq, Ord)
 
-data CellState = Free (Maybe Char) | Blocked
+data CellState = Free (Maybe Char) | Blocked deriving (Eq, Ord)
 
 data Cell = Cell
   { cellCoordinate :: Coordinate
   , cellState      :: CellState
-  }
+  } deriving (Eq, Ord)
 
 instance Show CellState where
   show Blocked         = "+"
@@ -25,7 +30,7 @@ parseCellState c
 
 data Grid = Grid
   { gridWidth :: Int
-  , gridCells :: [Cell]
+  , gridCells :: Set Cell
   }
 
 instance Semigroup Grid where
@@ -40,7 +45,7 @@ instance Show Grid where
           case filter (\cell -> cellCoordinate cell == Coordinate (x, y)) cells of
             [val] -> show $ cellState val
             _     -> error ""
-    in unlines . map (\y -> concatMap (\x -> displayCell (x, y) (gridCells g)) [0..wi]) $ [0..wi]
+    in unlines . map (\y -> concatMap (\x -> displayCell (x, y) (Set.toList $ gridCells g)) [0..wi]) $ [0..wi]
 
 allCoordinates :: Int -> [Coordinate]
 allCoordinates width = fmap Coordinate $ (,) <$> [0..width'] <*> [0..width']
@@ -49,25 +54,33 @@ allCoordinates width = fmap Coordinate $ (,) <$> [0..width'] <*> [0..width']
 -- | From a list of lines representing a grid, read the actual information from it
 parseGrid :: [String] -> Grid
 parseGrid lines =
-  foldr rowFold (Grid width []) $ zip [0..] lines
+  foldr rowFold (Grid width Set.empty) $ zip [0..] lines
   where
     width :: Int
     width = length lines 
     rowFold :: (Int, String) -> Grid -> Grid
-    rowFold (rowIndex, row) acc = acc <> foldr colFold (Grid width []) (zip [0..] row)
+    rowFold (rowIndex, row) acc = acc <> foldr colFold (Grid width Set.empty) (zip [0..] row)
       where
         colFold :: (Int, Char) -> Grid -> Grid
         colFold (columnIndex, col) acc' =
           let c = Coordinate (columnIndex, rowIndex)
               cell = Cell c (parseCellState col)
-          in acc' <> Grid width [cell]
+          in acc' <> Grid width (Set.fromList [cell])
 
 -- | Return true if the cell is solved, meaning it is a blocked cell or it has a character
 solvedCell :: Cell -> Bool
-solvedCell c = case cellState c of
-  Blocked       -> True
-  Free (Just _) -> True
-  _             -> False
+solvedCell c =
+  case cellState c of
+    Blocked       -> True
+    Free (Just _) -> True
+    _             -> False
+
+-- | Return true if the cell is free to be completed (or already completed)
+freeCell :: Cell -> Bool
+freeCell c = 
+  case cellState c of
+    Free _ -> True
+    _      -> False
 
 -- | Return true if all its cell are solved,
 -- it's a naive function as it won't check if the given words are actually there
@@ -75,16 +88,36 @@ solvedCell c = case cellState c of
 solvedGrid :: Grid -> Bool
 solvedGrid = all solvedCell . gridCells
 
-data SegmentOrientation = Down | Right
+data SegmentOrientation = Vertical | Horizontal deriving (Eq)
 
 data Segment = Segment
-  { segmentCells :: [Cell]
-  , orientation  :: SegmentOrientation
-  }
+  { segmentCells       :: Set Cell
+  , segmentOrientation :: SegmentOrientation
+  } deriving (Eq)
 
 -- | From a grid, determine all its segments
 segments :: Grid -> [Segment]
-segments = undefined
+segments g =
+  let w = gridWidth g
+      freeCells = Set.toList . Set.filter freeCell $ gridCells g
+      getSegments :: Cell -> [Segment]
+      getSegments c = nub $ mapMaybe (\c' -> trySegment c c' freeCells) freeCells
+      trySegment :: Cell -> Cell -> [Cell] -> Maybe Segment
+      trySegment c1 c2 allFreeCells
+        | x1 == x2 && abs (y1 - y2) == 1 =
+            return Segment
+              { segmentOrientation = Vertical
+              , segmentCells = undefined
+              }
+        | y1 == y2 && abs (x1 - x2) == 1 =
+            return Segment
+              { segmentOrientation = Horizontal
+              , segmentCells = undefined
+              }
+        | otherwise = Nothing
+        where (x1, y1) = coordinateValue . cellCoordinate $ c1
+              (x2, y2) = coordinateValue . cellCoordinate $ c2
+  in concatMap getSegments freeCells
 
 -- | Apply a string to a segment, will return a segment on success
 applyWordToSegment :: Segment -> String -> Maybe Segment
@@ -107,7 +140,7 @@ applySegments = undefined
 -- | From non-completed grid and a list of words, will return a soluce list of grid
 -- or an error if it can't find a solution, which shouldn't happen
 solve :: Grid -> [String] -> Grid
-solve grid words = undefined
+solve grid words = grid
 
 main :: IO ()
 main = interact $
