8bcef5c9bd7164dc65adf9b1f905d5cb2306dd66
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2021-11-09 23:28:56 +0000

    mudança do valor de base

--- FunctionalProgramming/InterpreterAndCompilers/intuitive-language.hs
+++ FunctionalProgramming/InterpreterAndCompilers/intuitive-language.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Map  (Map)
+import Data.Ratio (Rational)
 import Data.Text (Text)
 
-import qualified Data.Map  as Map
-import qualified Data.Text as Text
+import qualified Data.Map   as Map
+import qualified Data.Ratio as Ratio
+import qualified Data.Text  as Text
 
 newtype Key = Key Text
 
-data Value = Number Integer
-  | RationalNumber Integer Integer
-  | Function Int [Value]
+createKey :: Text -> Maybe Key
+createKey t = undefined
+
+data Value = Number Rational | Function [Value]
 
 newtype Memory = Memory { memory :: Map Key Value }
